fd65ae30f41c9b6f382b63d8f6c29d810fa90c10
Julie Quinn <Julie@first-interstate.local>
2017-06-30 12:55:00 -0400

    Friday changes

--- Assets/JS/newLand.js
+++ Assets/JS/newLand.js
@@ -11,24 +11,18 @@ firebase.initializeApp(config);
 
 var ref = firebase.database();
 
+// Require geocoder
+var geocoder = require('./node_modules/geocoder');
+
 // Location array
 ref.ref().on("value", function(snapshot) {
     var locations = snapshot.val();
     for (var key in locations) {
         var location = locations[key];
         console.log(location);
-        geocodeAddress(location);
-    }
-})
+        geocoder.geocode(location, function (err, data){
 
-// Geocode addresses from customer data table on index.html
-function geocodeAddress(location) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-        'address': location.street1 + ' ' + location.city + ' ' + location.state + ' ' + location.zip
-    }, function(results, status) {
-        // Drop a pin on map for each geocoded address
-        console.log(status);
+
         if (status == 'OK') {
             window.mapInstance.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
@@ -42,29 +36,24 @@ function geocodeAddress(location) {
             // Window labels
             var infowindow =  new google.maps.InfoWindow({
                 content: location.name + "<br>" + location.street1 + ' ' + location.street2 + "<br>" + location.city + ' ' + location.state + ' ' + location.zip,
+                // map: map
             });
 
             // Display window labels when marker clicked
             google.maps.event.addListener(marker, 'click', function() {
                 infowindow.open(map, this);
             });
-        if (status == 'OVER_QUERY_LIMIT') {
-            
-
-        }
 
             // Error alert
         } else {
             alert("geocode of " + location.name + " failed:" + status);
         }
-    });
-}
 
-// Draw radius variables
-var map;
-var radiusHalfMile;
-var radiusMile;
-var radiusMileHalf; 
+        })
+    }
+})
+
+
 
 // Map settings
 function initMap() {
@@ -112,9 +101,3 @@ function initMap() {
 
     window.mapInstance = map;
 };
-
-
-
-
-
-
